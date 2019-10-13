a134f93eb0c1c2c9e5d360c730b5d938d89ab452
Max <dragoonz@yandex.ru>
2019-10-13 08:25:27 +0300

    join session button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			appStatus: "OW" //'start', 'OW', 'EAP', 'TLP'
+			appStatus: "start" //'start', 'OW', 'EAP', 'TLP'
 		};
 	}
 	handleGameChange = nextGame => {
