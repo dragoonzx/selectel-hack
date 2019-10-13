18a971e1723148a8a99ba4503b1dce86e74cfb8c
Max <dragoonz@yandex.ru>
2019-10-13 13:36:43 +0300

    fdss

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			appStatus: "EAP" //'start', 'OW', 'EAP', 'TLP', 'final'
+			appStatus: "OW" //'start', 'OW', 'EAP', 'TLP', 'final'
 		};
 	}
 	handleGameChange = nextGame => {
